ed41fec4e026b4b44ee676e70ee4bbd09c4a2f9f
반선호 <shakevan@woowahan.com>
2022-03-27 15:42:18 +0900

    week 5 : logging

--- kvp-spring-boot-web/src/main/kotlin/kvp/van/springboot/VanRunner.kt
+++ kvp-spring-boot-web/src/main/kotlin/kvp/van/springboot/VanRunner.kt
@@ -1,13 +1,19 @@
 package kvp.van.springboot
 
 import kvp.van.springboot.van.Van
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 
 @Component
 class VanRunner(val van: Van): ApplicationRunner {
+    private val logger: Logger = LoggerFactory.getLogger("kvp.van")
+
     override fun run(args: ApplicationArguments?) {
+        logger.debug("hello!! debug!")
+        logger.info("hello!! info!")
         println("van age : ${van.age}, name : ${van.name}" )
     }
 }
